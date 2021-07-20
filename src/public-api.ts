607c23443c0297dff76c077c22649924f6bc19aa
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-07-20 22:08:15 +0200

    chore(api): add api

---
+++ src/public-api.ts
@@ -0,0 +1,24 @@
+/*
+ * Public API Surface of core
+ */
+/**
+ * Callback
+ * --------
+ * @angular-package/core/callback
+ */
+export {
+  // Class.
+  Callback,
+} from './callback';
+
+/**
+ * Error
+ * -----
+ * @angular-package/core/error
+ */
+export {
+  // Class.
+  ValidationError,
+  // Interface.
+  ErrorMessage,
+} from './error';
